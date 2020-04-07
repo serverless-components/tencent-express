d682b66ab7858bb85c29548f90514056f8c34749
yugasun <yuga_sun@163.com>
2020-04-07 19:36:20 +0800

    fix: package version

--- release.config.js
+++ release.config.js
@@ -44,7 +44,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['package.json', 'src/**', 'package-lock.json', 'CHANGELOG.md'],
+        assets: ['package.json', 'src/**', 'CHANGELOG.md'],
         message: 'chore(release): version ${nextRelease.version} \n\n${nextRelease.notes}'
       }
     ],
