fa2ee0c2b30575779cb69ef49af345ffd24bf631
yugasun <yuga_sun@163.com>
2020-04-21 12:11:50 +0800

    fix: add compatibility for node10

--- src/shims/handler.js
+++ src/shims/handler.js
@@ -2,6 +2,9 @@ const { createServer, proxy } = require('tencent-serverless-http')
 
 module.exports.handler = (event, context) => {
   const app = require.fromParentEnvironment('./app')
+  // compatibility for node10
+  context.callbackWaitsForEmptyEventLoop =
+    app.callbackWaitsForEmptyEventLoop === false ? false : true
   const server = createServer(app, null, app.binaryTypes || [])
   return proxy(server, event, context, 'PROMISE').promise
 }
