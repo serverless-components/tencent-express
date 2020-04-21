b407e1a192ca4b6fbc6fc05ee71a0adf0384771c
yugasun <yuga_sun@163.com>
2020-04-21 17:15:52 +0800

    fix: make callbackWaitsForEmptyEventLoop default false

--- src/shims/handler.js
+++ src/shims/handler.js
@@ -2,9 +2,10 @@ const { createServer, proxy } = require('tencent-serverless-http')
 
 module.exports.handler = (event, context) => {
   const app = require.fromParentEnvironment('./app')
-  // compatibility for node10
+
   context.callbackWaitsForEmptyEventLoop =
-    app.callbackWaitsForEmptyEventLoop === false ? false : true
+    app.callbackWaitsForEmptyEventLoop === true ? true : false
+
   const server = createServer(app, null, app.binaryTypes || [])
   return proxy(server, event, context, 'PROMISE').promise
 }
