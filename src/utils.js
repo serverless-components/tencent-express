1b2cf9d7c890a94e7deaa830814b9758ac42cdf4
yugasun <yuga_sun@163.com>
2020-05-06 17:27:41 +0800

    fix: read bucket and object from srcOriginal

--- src/utils.js
+++ src/utils.js
@@ -149,12 +149,11 @@ const prepareInputs = async (instance, credentials, inputs = {}) => {
   const stateServiceId = instance.state[regionList[0]] && instance.state[regionList[0]].serviceId
 
   const functionConf = {
-    code:
-      typeof inputs.src === 'object'
-        ? inputs.src
-        : {
-            src: inputs.src
-          },
+    code: {
+      src: inputs.src,
+      bucket: inputs.srcOriginal && inputs.srcOriginal.bucket,
+      object: inputs.srcOriginal && inputs.srcOriginal.object
+    },
     name:
       ensureString(inputs.functionName, { isOptional: true }) ||
       stateFunctionName ||
