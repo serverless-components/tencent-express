7c2827162f94b817f5e8f3c00e279263bff120e7
yugasun <yuga_sun@163.com>
2020-07-06 11:02:43 +0800

    feat: add integration test

---
+++ tests/integration.test.js
@@ -0,0 +1,63 @@
+const { generateId, getServerlessSdk } = require('./utils')
+const execSync = require('child_process').execSync
+const path = require('path')
+const axios = require('axios')
+
+// set enough timeout for deployment to finish
+jest.setTimeout(300000)
+
+// the yaml file we're testing against
+const instanceYaml = {
+  org: 'orgDemo',
+  app: 'appDemo',
+  component: 'express@dev',
+  name: `express-integration-tests-${generateId()}`,
+  stage: 'dev',
+  inputs: {
+    region: 'ap-guangzhou',
+    runtime: 'Nodejs8.9',
+    apigatewayConf: { environment: 'test' }
+  }
+}
+
+// get credentials from process.env but need to init empty credentials object
+const credentials = {
+  tencent: {}
+}
+
+// get serverless construct sdk
+const sdk = getServerlessSdk(instanceYaml.org)
+
+it('should successfully deploy express app', async () => {
+  const instance = await sdk.deploy(instanceYaml, { tencent: {} })
+
+  expect(instance).toBeDefined()
+  expect(instance.instanceName).toEqual(instanceYaml.name)
+  // get src from template by default
+  expect(instance.outputs.templateUrl).toBeDefined()
+  expect(instance.outputs.region).toEqual(instanceYaml.inputs.region)
+  expect(instance.outputs.apigw).toBeDefined()
+  expect(instance.outputs.apigw.environment).toEqual(instanceYaml.inputs.apigatewayConf.environment)
+  expect(instance.outputs.scf).toBeDefined()
+  expect(instance.outputs.scf.runtime).toEqual(instanceYaml.inputs.runtime)
+})
+
+it('should successfully update source code', async () => {
+  // change source to own source './src' and need to install packages before deploy
+  const srcPath = path.join(__dirname, 'src')
+  execSync('npm install', { cwd: srcPath })
+  instanceYaml.inputs.src = srcPath
+
+  const instance = await sdk.deploy(instanceYaml, credentials)
+  const response = await axios.get(instance.outputs.apigw.url)
+
+  expect(response.data).toEqual('hello world')
+  expect(instance.outputs.templateUrl).not.toBeDefined()
+})
+
+it('should successfully remove express app', async () => {
+  await sdk.remove(instanceYaml, credentials)
+  result = await sdk.getInstance(instanceYaml.org, instanceYaml.stage, instanceYaml.app, instanceYaml.name)
+
+  expect(result.instance.instanceStatus).toEqual('inactive')
+})
